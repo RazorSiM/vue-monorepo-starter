f35725586f890c064eda23b20452c07539399bc5
Raz <me@raz.wtf>
2023-03-14 18:08:22 +0100

    remove unused comments

--- apps/vue-spa/unocss.config.ts
+++ apps/vue-spa/unocss.config.ts
@@ -1,25 +1,3 @@
-// import {
-//   defineConfig,
-//   presetIcons,
-//   presetTypography,
-//   presetUno,
-//   presetWebFonts,
-//   transformerVariantGroup,
-// } from "unocss";
-
-// export default defineConfig({
-//   presets: [
-//     presetUno(),
-//     presetIcons(),
-//     presetWebFonts({
-//       provider: "google",
-//       fonts: { sans: "Roboto", mono: "Fira Code" },
-//     }),
-//     presetTypography(),
-//   ],
-//   transformers: [transformerVariantGroup()],
-// });
-
 import config from "@vue-monorepo/unocss";
 export default {
   ...config,
