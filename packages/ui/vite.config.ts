d262370b3f32f6568a2590318d83dc2a201da801
Raz <me@raz.wtf>
2023-03-14 22:01:15 +0100

    clean

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import unocss from "unocss/vite";
-import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), unocss()],
-  resolve: {
-    // alias: {
-    //   "@": fileURLToPath(new URL("./src", import.meta.url)),
-    // },
-  },
 });
--- packages/vue-utils-lib/vite-library.config.ts
+++ packages/vue-utils-lib/vite-library.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
       skipDiagnostics: true,
-      clearPureImport: false,
+      clearPureImport: true,
       cleanVueFileName: true,
     }),
   ],
