b694f29692ef2b3f5f9f3ac6b9d8279ba0500a84
Raz <me@raz.wtf>
2023-06-14 17:24:01 +0200

    add eslint config for unocss

--- packages/eslint-config-vue/index.cjs
+++ packages/eslint-config-vue/index.cjs
@@ -1,16 +1,16 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: ["turbo", "@antfu"],
+  extends: ['turbo', '@antfu', '@unocss'],
   overrides: [
     {
-      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
     },
   ],
   rules: {
-    "@typescript-eslint/no-namespace": "off",
+    '@typescript-eslint/no-namespace': 'off',
   },
-};
+}
